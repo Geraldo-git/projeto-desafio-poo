8c316b3a5041143fc9e1a1a22b0bb8aeed26be0e
Geraldo-git <laboratorio.inicial@gmail.com>
2021-12-21 12:25:57 -0300

    Implementacao dos metodos da classe Dev

--- src/br/com/dio/desafio/Bootcamp.java
+++ src/br/com/dio/desafio/Bootcamp.java
@@ -13,7 +13,7 @@ public class Bootcamp {
     private LocalDate dataInicial = LocalDate.now();
     private final LocalDate dataFinal = dataInicial.plusDays(45);
 
-    private Set<Dev> devs = new HashSet<>();
+    private Set<Dev> devsInscritos = new HashSet<>();
     private Set<Conteudo> conteudos = new LinkedHashSet<>();
 
     public String getNome() {
@@ -44,12 +44,12 @@ public class Bootcamp {
         return dataFinal;
     }
 
-    public Set<Dev> getDevs() {
-        return devs;
+    public Set<Dev> getDevsInscritos() {
+        return devsInscritos;
     }
 
-    public void setDevs(Set<Dev> devs) {
-        this.devs = devs;
+    public void setDevsInscritos(Set<Dev> devsInscritos) {
+        this.devsInscritos = devsInscritos;
     }
 
     public Set<Conteudo> getConteudos() {
@@ -65,11 +65,11 @@ public class Bootcamp {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Bootcamp bootcamp = (Bootcamp) o;
-        return Objects.equals(nome, bootcamp.nome) && Objects.equals(descricao, bootcamp.descricao) && Objects.equals(dataInicial, bootcamp.dataInicial) && Objects.equals(dataFinal, bootcamp.dataFinal) && Objects.equals(devs, bootcamp.devs) && Objects.equals(conteudos, bootcamp.conteudos);
+        return Objects.equals(nome, bootcamp.nome) && Objects.equals(descricao, bootcamp.descricao) && Objects.equals(dataInicial, bootcamp.dataInicial) && Objects.equals(dataFinal, bootcamp.dataFinal) && Objects.equals(devsInscritos, bootcamp.devsInscritos) && Objects.equals(conteudos, bootcamp.conteudos);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(nome, descricao, dataInicial, dataFinal, devs, conteudos);
+        return Objects.hash(nome, descricao, dataInicial, dataFinal, devsInscritos, conteudos);
     }
 }
