9d898c8e82b6915b992a1a8af1a167b416dbc96f
Geraldo-git <laboratorio.inicial@gmail.com>
2021-12-21 10:47:21 -0300

    Implemetacao classes Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import br.com.dio.desafio.Curso;
+import br.com.dio.desafio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        Curso curso2 = new Curso();
+
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descricao curso java");
+        curso1.setCargaHoraria(8);
+
+        curso2.setTitulo("curso javascript");
+        curso2.setDescricao("descricao curso javascript");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descricao mentoria de Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+    }
+}
